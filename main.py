04c73d055fd51529718853fcc35c753e982e0cc8
Nader7x <elnagarnader23@gmail.com>
2023-11-29 19:21:20 +0200

    test

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# import nltk
+# nltk.download('punkt')
+# Tokenization using NLTK
+from nltk import word_tokenize, sent_tokenize
+sent = "GeeksforGeeks is a great learning platform.\
+It is one of the best for Computer Science students."
+print(word_tokenize(sent))
+print(sent_tokenize(sent))
+print("xd")
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
