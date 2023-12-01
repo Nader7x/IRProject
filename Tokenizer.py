9cae3f1d26cc71cd7a1819a70468c7f7a851dd0e
Nader7x <elnagarnader23@gmail.com>
2023-12-01 14:34:19 +0200

    commit

---
+++ Tokenizer.py
@@ -0,0 +1,5 @@
+import nltk
+def Toknize():
+    {
+       nltk.tokenize()
+    }
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 # Tokenization using NLTK
 from nltk import word_tokenize, sent_tokenize
+
+import Tokenizer
+
 sent = "GeeksforGeeks is a great learning platform.\
 It is one of the best for Computer Science students."
 print(word_tokenize(sent))
 print(sent_tokenize(sent))
+Tokenizer.myfunction()
